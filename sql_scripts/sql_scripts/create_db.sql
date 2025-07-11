6ae43e6ca58106fe1556c92dbca520e16e0f6be1
Robinson Ramirez <robinsonramirez@MacBookAir.lan>
2025-07-11 11:55:03 -0500

    ajustes

--- sql_scripts/sql_scripts/create_db.sql
+++ sql_scripts/sql_scripts/create_db.sql
@@ -14,7 +14,7 @@ CREATE TABLE province (
 
 INSERT INTO province (id, name) VALUES
 (1, 'Cundinamarca'), (2, 'Antioquia'), (3, 'Choco'),
-(4, 'Boyaca'), (5, 'Casanare'), (6, 'Pasto'), (7, 'Quindio');
+(4, 'Boyaca'), (5, 'Casanare');
 
 -- Tabla: canton
 CREATE TABLE canton (
@@ -27,7 +27,10 @@ CREATE TABLE canton (
 INSERT INTO canton (id, name, province_id) VALUES
 (1, 'Bogota', 1), (2, 'Medellin', 1), (3, 'Barranquilla', 1),
 (4, 'Cali', 2), (5, 'Tunja', 2), (6, 'Cartagena', 2),
-(7, 'Santa Marta', 3), (8, 'Valledupar', 3), (9, 'Monteria', 3);
+(7, 'Santa Marta', 3), (8, 'Valledupar', 3), (9, 'Monteria', 3),
+(10, 'Pasto', 4), (11, 'Pereira', 4), (12, 'Villavicencio', 4),
+(13, 'Quito', 5), (14, 'Cucuta', 5), (15, 'Mocoa', 5);
+
 
 -- Tabla: district
 CREATE TABLE district (
@@ -38,9 +41,12 @@ CREATE TABLE district (
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO district (id, name, canton_id) VALUES
-(1, 'Chia', 1), (2, 'Cajica', 1), (3, 'Cota', 1),
-(4, 'Tunjuelito', 2), (5, 'Facatativa', 2), (6, 'Fomeque', 2),
-(7, 'Tocancipa', 3), (8, 'Zipaquira', 3), (9, 'Suesca', 3);
+(1, 'Chia', 1), (2, 'Cajica', 2), (3, 'Cota', 3),
+(4, 'Tunjuelito', 4), (5, 'Facatativa', 5), (6, 'Fomeque', 6),
+(7, 'Tocancipa', 7), (8, 'Zipaquira', 8), (9, 'Suesca', 9),
+(10, 'Soacha', 10), (11, 'Mosquera', 11), (12, 'Madrid', 12),
+(13, 'Girardot', 13), (14, 'Melgar', 14), (15, 'Espinal', 15);
+
 
 -- Tabla: category
 CREATE TABLE category (
@@ -49,4 +55,7 @@ CREATE TABLE category (
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO category (id, name) VALUES
-(1, 'Terror'), (2, 'Romance'), (3, 'Fantasía'), (4, 'Historica'), (5, 'Misterio');
+(1, 'Terror'), (2, 'Romance'), (3, 'Fantasia'), (4, 'Historia'), (5, 'Misterio');
+
+
+
